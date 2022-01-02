3f7bdebe9de3b68e06d9e2d8eba44a35b1a94d92
Anupriya Rentala <r.anupriya007@gmail.com>
2022-01-02 13:01:59 -0800

    day 2

--- NEW/CyclicSort/findCorruptNumber.cpp
+++ NEW/CyclicSort/findCorruptNumber.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 void f(vector<int> arr){
     
-    for(int i=0; i<arr.size()-1;) {
+    for(int i=0; i<arr.size();) {
         if(arr[i]!= arr[arr[i]-1]) { //we checck if curr value is present at (value at index currVal - 1) Why dont we check if curVal is i+1 ?
-            int j = arr[i];
-            int t = arr[j-1];
-            arr[j-1] = j;
+            int t = arr[arr[i]-1];
+            arr[arr[i]-1] = arr[i];
             arr[i] = t;
         }
         else{
@@ -20,8 +19,12 @@ void f(vector<int> arr){
     }
 
     for(int i=0; i<arr.size(); i++){
-        if(arr[i]!= i+1)
-            cout<<i+1<<" ";
+        if(arr[i]!= i+1){
+            cout<<"Duplicated value: "<<arr[i]<<endl;
+
+            cout<<"Missing value: "<<i+1<<endl;
+        }
+            
     }
     cout<<endl;
 }
@@ -29,6 +32,9 @@ void f(vector<int> arr){
 int main(){
     
     f({2,4,1,2});
+    //f({3,1,2,5,2});
+
+
     //f({2,3,1,8,2,3,5,1});
 
 }
